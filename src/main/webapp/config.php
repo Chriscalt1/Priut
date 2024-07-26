e8b7e8e5836a0769c5cda24375e853648fa3762d
Chistina <cool.musakrisa@yandex.ru>
2024-07-27 00:06:06 +0700

    Final version

--- src/main/webapp/config.php
+++ src/main/webapp/config.php
@@ -6,19 +6,28 @@ $dbname = "priut";
 
 $priut = new mysqli($servername, $username, $password, $dbname);
 
+if ($priut->connect_error) {
+    die("Connection failed: " . $priut->connect_error);
+}
 
-$limit = isset($_GET['limit']) ? intval($_GET['limit']) : 9;
+$limit = isset($_GET['limit']) ? intval($_GET['limit']) : 50;
 $offset = isset($_GET['offset']) ? intval($_GET['offset']) : 0;
 
-$sql = "SELECT id, имя, возраст, вид, изображение, краткое_описание, здоров FROM animals";
-$result = $priut->query($sql);
+$sql = "SELECT id, имя, возраст, вид, изображение, краткое_описание, здоров FROM animals LIMIT ?, ?";
+$stmt = $priut->prepare($sql);
+$stmt->bind_param("ii", $offset, $limit);
+$stmt->execute();
+$result = $stmt->get_result();
 
 if ($result->num_rows > 0) {
     echo '<div class="card-container">';
     while($row = $result->fetch_assoc()) {
+        $imageData = base64_encode($row["изображение"]);
+        $imageSrc = 'data:image/jpeg;base64,' . $imageData;
+
         echo '
         <div class="card">
-            <img src="' . $row["изображение"] . '" alt="Image">
+            <img src="' . $imageSrc . '" alt="Image">
             <h2>Имя: ' . $row["имя"] . '</h2>
             <p>Возраст: ' . $row["возраст"] . '</p>
             <p>Вид: ' . $row["вид"] . '</p>
@@ -31,7 +40,10 @@ if ($result->num_rows > 0) {
     echo "0 results";
 }
 
+$stmt->close();
 $priut->close();
+?>
+
 
 
 
